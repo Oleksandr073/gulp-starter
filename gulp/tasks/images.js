4d109835864be6a08dab3cbf669bb9e0419b7471
Oleksandr073 <102832044+Oleksandr073@users.noreply.github.com>
2023-03-29 22:51:32 +0300

    update webp convert

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,13 +3,19 @@ import { path } from "../config/path.js";
 import { plugins } from "../config/plugins.js";
 import { isBuild } from "../constants/isBuild.js";
 
-import webp from "gulp-webp";
 import imagemin, { mozjpeg, optipng, svgo } from "gulp-imagemin";
+import imageminWebp from "imagemin-webp";
+import extReplace from "gulp-ext-replace";
 
 export function images() {
     return gulp.src(path.src.images)
         .pipe(plugins.newer(path.build.images))
-        .pipe(webp())
+        .pipe(imagemin([
+            imageminWebp({
+                quality: 90,
+            }),
+        ]))
+        .pipe(extReplace('.webp'))
         .pipe(gulp.dest(path.build.images))
         .pipe(gulp.src(path.src.images))
         .pipe(plugins.newer(path.build.images))
